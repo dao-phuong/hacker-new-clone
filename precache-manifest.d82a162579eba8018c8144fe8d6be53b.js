7e37ab3a47f83b492e46f4d3ee3908a81ce41a30
Dao Duy Phuong <daoduyphuong0995@gmail.com>
2019-08-23 16:36:41 +0700

    Updates

---
+++ precache-manifest.d82a162579eba8018c8144fe8d6be53b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "521ca2bf704cf756af2fc9681402eda1",
+    "url": "/hacker-new-clone/index.html"
+  },
+  {
+    "revision": "73068065999a0ceea3ae",
+    "url": "/hacker-new-clone/static/js/2.04b72771.chunk.js"
+  },
+  {
+    "revision": "a4d1e05e8dc318186bf0",
+    "url": "/hacker-new-clone/static/js/main.6626e158.chunk.js"
+  },
+  {
+    "revision": "e62288a4db092b0a85a7",
+    "url": "/hacker-new-clone/static/js/runtime~main.8727b5c1.js"
+  }
+]);
